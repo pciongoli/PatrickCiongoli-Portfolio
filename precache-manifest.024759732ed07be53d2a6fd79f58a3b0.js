1473e347b236fd6a180ac2cf92f585c4b1b278ef
pcion <patrick.ciongoli@gmail.com>
2022-06-05 22:50:12 -0400

    Updates

---
+++ precache-manifest.024759732ed07be53d2a6fd79f58a3b0.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "976aa56c6603f45020d1",
+    "url": "/PatrickCiongoli-Portfolio/static/css/main.b353dfc8.chunk.css"
+  },
+  {
+    "revision": "976aa56c6603f45020d1",
+    "url": "/PatrickCiongoli-Portfolio/static/js/main.976aa56c.chunk.js"
+  },
+  {
+    "revision": "720485474eeb78d66576",
+    "url": "/PatrickCiongoli-Portfolio/static/js/1.72048547.chunk.js"
+  },
+  {
+    "revision": "d2c47a920e3f6302e8d0",
+    "url": "/PatrickCiongoli-Portfolio/static/js/runtime~main.d2c47a92.js"
+  },
+  {
+    "revision": "9708ab59283fc86eb2e8ceff832a5aa4",
+    "url": "/PatrickCiongoli-Portfolio/static/media/cover-image.9708ab59.jpg"
+  },
+  {
+    "revision": "a404b54bff99fdca006e2858ac1c5d4a",
+    "url": "/PatrickCiongoli-Portfolio/static/media/4.a404b54b.jpg"
+  },
+  {
+    "revision": "732b1c9a715504910e6ccc25a8796462",
+    "url": "/PatrickCiongoli-Portfolio/static/media/PC-MVC-TechBlog.732b1c9a.jpg"
+  },
+  {
+    "revision": "22ddfceb32c495006d343c0690a1e07c",
+    "url": "/PatrickCiongoli-Portfolio/static/media/deep-thoughts.22ddfceb.jpg"
+  },
+  {
+    "revision": "5117a5dae4be7474fe7cae896c0d60c3",
+    "url": "/PatrickCiongoli-Portfolio/static/media/pc-BudgetTracker.5117a5da.jpg"
+  },
+  {
+    "revision": "d389fc97aeb1611b5a8d49001966d66a",
+    "url": "/PatrickCiongoli-Portfolio/static/media/zookeeper.d389fc97.jpg"
+  },
+  {
+    "revision": "6f31f8003e182de00109647d52201e1e",
+    "url": "/PatrickCiongoli-Portfolio/index.html"
+  }
+];
