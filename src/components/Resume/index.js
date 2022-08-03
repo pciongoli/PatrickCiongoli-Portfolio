d8b35d3067146ada46bbd6f7c1dfd451c7dd1aaa
pcion <patrick.ciongoli@gmail.com>
2022-08-03 10:38:36 -0400

    updated resume branding statement and contact

--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -2,38 +2,156 @@ import React from "react";
 
 function Resume() {
    return (
-      <section className="my-5">
-         <div className="my-2">
-            <p>
-               Download my{" "}
-               <a href="https://docs.google.com/document/d/1he3R8m0ufMgwWjej5prnGR-8SdXBbw3KA6lRCbZOJBs/edit?usp=sharing">
-                  resume
-               </a>
-            </p>
-            <h3>Front-end Proficiencies</h3>
-            <ul className="skills">
-               <li>HTML</li>
-               <li>CSS</li>
-               <li>JavaScript</li>
-               <li>jQuery</li>
-               <li>responsive design</li>
-               <li>React</li>
-               <li>Bootstrap</li>
-               <li>Foundation</li>
-            </ul>
-            <h3>Back-end Proficiencies</h3>
-            <ul className="skills">
-               <li>APIs</li>
-               <li>Node</li>
-               <li>Express</li>
-               <li>MySQL, Sequelize</li>
-               <li>MongoDB, Mongoose</li>
-               <li>OOP</li>
-               <li>REST</li>
-               <li>GraphQL</li>
-            </ul>
-         </div>
-      </section>
+      <div>
+         <p>
+            <strong>Patrick Ciongoli</strong>
+         </p>
+         <p></p>
+
+         <p>
+            Ambitious full stack web developer with a large spectrum of
+            knowledge in both front and backend languages and procedures. I
+            received my bachelor's degree in economics with a minor in
+            psychology. After graduation, I continued my education through
+            Rutgers University Web Development Boot Camp in which I received my
+            Certificate in Full Stack Web Development. I am seeking
+            opportunities in which I can fully embrace my passion as a full
+            stack web developer.&nbsp;
+         </p>
+         <p></p>
+         <p>
+            <strong>TECHNICAL SKILLS</strong>
+         </p>
+         <ul>
+            <li>
+               <strong>Frontend</strong>: HTML5, AJAX, Handlebars, JSX, CSS,
+               JQuery, JavaScript, Bootstrap, Foundation, SEO
+            </li>
+            <li>
+               <strong>Backend</strong>: MERN, MongoDB, Express.js, React.js,
+               Node, APIs, REST, OOP, ORM, Jest, TDD, MVC, User Authentication,
+               Stripe, IndexDB
+            </li>
+         </ul>
+         <p></p>
+         <p>
+            <strong>SKILLS</strong>
+         </p>
+         <ul>
+            <li>Excellent verbal and written communication skills</li>
+            <li>
+               Flexible, quick learner, focused, dependable, self-motivated, and
+               detail-oriented
+            </li>
+            <li>Strong problem solving skills</li>
+            <li>
+               Able to effectively self manage while completing individual
+               projects, as well as collaborate and contribute in a team
+               setting&nbsp;
+            </li>
+            <li>Proficiency in Git and Microsoft Office</li>
+         </ul>
+         <p></p>
+         <p>
+            <strong>EDUCATION</strong>
+         </p>
+         <p>
+            <strong>Rutgers University - &nbsp;</strong>NJ
+            <strong>
+               &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
+               &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
+               &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;<em>2022</em>
+            </strong>
+         </p>
+         <ul>
+            <li>
+               Full Stack Web Development Bootcamp Certificate &nbsp; &nbsp;
+               &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
+               &nbsp;&nbsp;&nbsp; &nbsp;&nbsp; &nbsp; &nbsp;
+            </li>
+         </ul>
+         <p>
+            <strong>Rutgers University -&nbsp;</strong> Newark, NJ
+            <strong>
+               &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
+               &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
+               &nbsp; &nbsp; &nbsp;<em>2021</em>
+            </strong>
+         </p>
+         <ul>
+            <li>
+               Bachelor of Arts, Economics &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
+               &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
+               &nbsp; &nbsp; &nbsp; &nbsp;
+            </li>
+            <li>Minor, Psychology</li>
+         </ul>
+         <p></p>
+         <p>
+            <strong>AWARDS</strong>
+         </p>
+         <p>
+            <strong>
+               Rutgers University Coding Bootcamp &nbsp; &nbsp; &nbsp; &nbsp;
+               &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
+               &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;2022
+            </strong>
+         </p>
+         <ul>
+            <li>"Wiz with the Front-End Skills"</li>
+         </ul>
+         <p></p>
+         <p>
+            <strong>WORK EXPERIENCE</strong>
+         </p>
+         <p>
+            <strong>
+               Corporate Tax Coordinator &nbsp;• &nbsp;August 2018 -
+               Present&nbsp;
+            </strong>
+         </p>
+         <p>
+            <em>
+               First Citizen Bank &nbsp;- &nbsp;CIT Group Inc., Morristown, NJ
+            </em>
+         </p>
+         <ul>
+            <li>
+               Preparation of corporate state and local income and franchise tax
+               returns
+            </li>
+            <li>
+               Produce annual reports, estimates and extensions for each
+               entity&nbsp;
+            </li>
+            <li>
+               Responsible for resolving state notices and processing
+               refunds&nbsp;
+            </li>
+            <li>
+               Involved with all necessary procedures to ensure subsidiaries
+               receive proper tax clearance&nbsp;
+            </li>
+            <li>
+               Work closely with legal team to advance the dissolution and
+               withdrawal of entities in business activities ceased&nbsp;
+            </li>
+         </ul>
+
+         <ul>
+            <li>
+               Mentor and train new tax coordinators to develop technical and
+               leadership skills
+            </li>
+            <li>
+               Working knowledge of requisite software and internal work
+               flow/procedures
+            </li>
+         </ul>
+         <p>
+            <br />
+         </p>
+      </div>
    );
 }
 
